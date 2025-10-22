da63eaff9fb89e4068f05e2bb2bfb51059628cc5
Evan Heringer <evanheringer@Evans-MacBook-Pro.local>
2025-10-21 22:59:12 -0700

    fix dbt models

--- dbt/dbt_jaffle_cosmos/models/marts/dim_customers.sql
+++ dbt/dbt_jaffle_cosmos/models/marts/dim_customers.sql
@@ -1,7 +1,6 @@
 select
   c.CUSTOMER_ID,
   c.FULL_NAME,
-  c.EMAIL,
   min(o.ORDER_DATE) as FIRST_ORDER_DATE,
   max(o.ORDER_DATE) as MOST_RECENT_ORDER_DATE,
   count(distinct o.ORDER_ID) as ORDER_COUNT
