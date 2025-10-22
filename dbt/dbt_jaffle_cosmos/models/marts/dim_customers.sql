c751fd06a1d2840867283fe3eff5e4313dd8ffca
Evan Heringer <evanheringer@Evans-MacBook-Pro.local>
2025-10-22 08:58:39 -0700

    update DBT model

--- dbt/dbt_jaffle_cosmos/models/marts/dim_customers.sql
+++ dbt/dbt_jaffle_cosmos/models/marts/dim_customers.sql
@@ -1,8 +1,12 @@
 select
   c.CUSTOMER_ID,
   c.FULL_NAME,
-  min(o.ORDER_DATE) as FIRST_ORDER_DATE,
-  max(o.ORDER_DATE) as MOST_RECENT_ORDER_DATE,
-  count(distinct o.ORDER_ID) as ORDER_COUNT
+  min(o.ORDER_DATE)              as FIRST_ORDER_DATE,
+  max(o.ORDER_DATE)              as MOST_RECENT_ORDER_DATE,
+  count(distinct o.ORDER_ID)     as ORDER_COUNT
 from {{ ref('stg_customers') }} c
-left join {{ ref('stg_orders') }} o using (CUSTOMER_ID)
+left join {{ ref('stg_orders') }} o
+  on c.CUSTOMER_ID = o.CUSTOMER_ID
+group by
+  c.CUSTOMER_ID,
+  c.FULL_NAME,
