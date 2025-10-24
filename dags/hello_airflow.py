8f8e391844428c70dbba012858ab8c06df61a2d8
Evan Heringer <evanheringer@Evans-MacBook-Pro.local>
2025-10-24 10:32:20 -0700

    add hello dag

---
+++ dags/hello_airflow.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="hello_airflow",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,  # Run manually from the UI
+    catchup=False,
+) as dag:
+
+    start = BashOperator(
+        task_id="start",
+        bash_command="echo 'Starting the DAG...'"
+    )
+
+    hello = BashOperator(
+        task_id="say_hello",
+        bash_command="echo 'Hello, Airflow!'"
+    )
+
+    end = BashOperator(
+        task_id="end",
+        bash_command="echo 'All done!'"
+    )
+
+    start >> hello >> end
